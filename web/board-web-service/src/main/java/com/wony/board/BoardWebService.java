6007f9d0eba804a1fff4a412dccadb70b5e2bb42
jinwon.yang <jinwon.yang@tosspayments.com>
2021-12-04 04:07:06 +0900

    WebService 에 Trasaction Layer 추가 * 설계상 위험한 방식이나, webService 에서 사용자 dto 를 만들때, LazyLoading 이 필요한 경우가 발생할 수 있어, 부득이하게 JPA 의존성을 추가(Transactional 필요)

--- web/board-web-service/src/main/java/com/wony/board/BoardWebService.java
+++ web/board-web-service/src/main/java/com/wony/board/BoardWebService.java
@@ -6,16 +6,19 @@ import com.wony.domain.board.BoardComment;
 import com.wony.util.WonyStreamUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class BoardWebService {
     private final BoardService boardService;
 
+    @Transactional(readOnly = true)
     public List<BoardDto> findAll() {
         return boardService.findAll()
                 .stream().map(board -> BoardDto
@@ -27,22 +30,26 @@ public class BoardWebService {
                         .build()).collect(Collectors.toList());
     }
 
+    @Transactional(readOnly = true)
     public List<BoardDto> findAllByTitle(String title) {
         return StreamSupport.stream(boardService.findByTitle(title).spliterator(), false)
                 .map(this::fromBoard).collect(Collectors.toList());
     }
 
+    @Transactional(readOnly = true)
     public List<BoardDto> findByAllByTitleCustom(long boardId, String title) {
         return boardService.findByTitleCustom(boardId, title)
                 .stream().map(this::fromBoard).collect(Collectors.toList());
     }
 
+    @Transactional(readOnly = true)
     public BoardDto findById(long id) {
         return boardService.findById(id)
                 .map(this::fromBoard)
                 .orElseThrow();
     }
 
+    @Transactional(readOnly = true)
     public BoardDto write(BoardForm boardForm) {
         Board board = boardService.save(boardForm);
         return fromBoard(board);
